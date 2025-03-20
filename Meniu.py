da66d63dbd46b6ea7b5accf510e5b6302bae6267
Virlan Victor-Stefan <roxorvicecity@gmail.com>
2025-03-20 20:55:16 +0200

    Am adaugat butonul Filter odd si am implementat functionalitatea acestuia

--- Meniu.py
+++ Meniu.py
@@ -5,18 +5,27 @@ def add_numbers():
     entrytext=numbers.get().split(",")
     for x in entrytext:
         listofnumbers.append(int(x))
+def filter_odd():
+    resultOdd.clear()
+    for num in listofnumbers:
+        if num%2==1:
+            resultOdd.append(num)
+    resulttext.insert('1.0', str(resultOdd))
 root=Tk()
 listofnumbers=[]
 numbers=StringVar()
+resultOdd=[]
+
 content=ttk.Frame(root,width=600,height=480)
 label=ttk.Label(content,text="List of integers: ")
 listentry=ttk.Entry(content,textvariable=numbers)
 resulttext=Text(root,width=40,height=10)
-resulttext.insert('1.0','myText')
 content.grid(column=0,row=0,sticky=(N,S,E,W))
 label.grid(column=0,row=0)
 listentry.grid(column=1,row=0,columnspan=2,rowspan=2,sticky=(N,S,E,W))
 addbutton=ttk.Button(content,text="Add",command=add_numbers)
 addbutton.grid(column=5,row=0)
-resulttext.grid(column=0, row=3, columnspan=6, sticky=(N,S,E,W))
+oddbutton=ttk.Button(content,text="Filter odd",command=filter_odd)
+oddbutton.grid(column=5,row=3)
+resulttext.grid(column=0, row=3, columnspan=4, sticky=(N,S,E,W))
 root.mainloop()
