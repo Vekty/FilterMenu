7d917618ced26a3972d9897a1a73fa0700d6556e
Virlan Victor-Stefan <roxorvicecity@gmail.com>
2025-03-20 19:11:42 +0200

    Am adaugat entry-ul specific listei de intregi

--- Meniu.py
+++ Meniu.py
@@ -1,8 +1,11 @@
 from tkinter import *
 from tkinter import ttk
 root=Tk()
+numbers=StringVar()
 content=ttk.Frame(root,padding=(3,3,12,12))
 label=ttk.Label(content,text="List of integers: ")
+listentry=ttk.Entry(content,textvariable=numbers)
 content.grid(column=0,row=0,sticky=(N,S,E,W))
 label.grid(column=0,row=0)
+listentry.grid(column=1,row=0,columnspan=2,rowspan=2,sticky=(N,S,E,W))
 root.mainloop()
